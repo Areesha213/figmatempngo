7018389573cdb6326a3707445ef4c4bc059cdfdf
Areesha <126898750+Areesha213@users.noreply.github.com>
2024-07-02 15:07:40 +0500

    Update OurJourneySection.jsx

--- src/components/OurJourneySection.jsx
+++ src/components/OurJourneySection.jsx
@@ -9,14 +9,14 @@ const OurJourneySection = ({ className = "" }) => {
       <div className="h-[576px] w-[1280px] relative rounded-xl bg-gray-100 hidden max-w-full" />
       <div className="flex flex-col items-start justify-start pt-16 px-0 pb-0 box-border min-w-[488.2px] max-w-full mq450:pt-[42px] mq450:box-border mq750:min-w-full mq1050:flex-1">
         <div className="flex flex-col items-start justify-start gap-[16px] max-w-full">
-          <b className="relative tracking-[2px] uppercase inline-block min-w-[129px] z-[1]">{`OUR JOURNEY `}</b>
-          <h1 className="m-0 relative text-6xl leading-[58px] font-bold font-inherit inline-block max-w-full z-[1] mq450:text-10xl mq450:leading-[35px] mq1050:text-19xl mq1050:leading-[46px]">
+          <b className="relative tracking-[2px] uppercase inline-block min-w-[129px] z-[1] ">{`OUR JOURNEY `}</b>
+          <h1 className="m-0 relative text-6xl leading-[58px] font-bold font-inherit inline-block max-w-full z-[1] mq450:text-4xl mq450:leading-[35px] mq1050:text-12xl mq1050:leading-[46px]">
             How we raised 34M
           </h1>
-          <div className=" relative  text-gray-200 inline-block max-h-[130px] [word-break:break-word] z-[1]">{`The advent of online fundraising platforms and digital payment systems has made it easier for individuals from all walks of life to contribute to causes they care about and have the ability to make their small donations count and go far. Digital donations have expanded the reach of our organisation. `}</div>
-          <div className="w-[276px] flex flex-row items-start justify-between gap-[20px] text-5xl">
+          <div className=" relative  text-gray-200 inline-block max-h-[130px] [word-break:break-word] z-[1] mq450:text-base">{`The advent of online fundraising platforms and digital payment systems has made it easier for individuals from all walks of life to contribute to causes they care about and have the ability to make their small donations count and go far. Digital donations have expanded the reach of our organisation. `}</div>
+          <div className="w-[276px] flex flex-row items-start justify-between gap-[20px] text-5xl mq450:mt-24">
             <div className="flex flex-col items-start justify-start">
-              <div className="relative leading-[160%] font-medium inline-block min-w-[62px] z-[1] mq450:text-lgi mq450:leading-[31px]">
+              <div className="relative leading-[160%] font-medium inline-block min-w-[62px] z-[1] mq450:text-xl mq450:leading-[31px]">
                 34M+
               </div>
               <div className="relative text-xs leading-[160%] font-medium text-gray-200 inline-block min-w-[101px] z-[1]">
@@ -24,7 +24,7 @@ const OurJourneySection = ({ className = "" }) => {
               </div>
             </div>
             <div className="flex flex-col items-start justify-start">
-              <div className="relative leading-[160%] font-medium inline-block min-w-[55px] z-[1] mq450:text-lgi mq450:leading-[31px]">
+              <div className="relative leading-[160%] font-medium inline-block min-w-[55px] z-[1] mq450:text-xl mq450:leading-[31px]">
                 400+
               </div>
               <div className="relative text-xs leading-[160%] font-medium text-gray-200 inline-block min-w-[52px] z-[1]">
@@ -32,7 +32,7 @@ const OurJourneySection = ({ className = "" }) => {
               </div>
             </div>
             <div className="flex flex-col items-start justify-start">
-              <div className="relative leading-[160%] font-medium inline-block min-w-[56px] z-[1] mq450:text-lgi mq450:leading-[31px]">
+              <div className="relative leading-[160%] font-medium inline-block min-w-[56px] z-[1] mq450:text-xl mq450:leading-[31px]">
                 15K+
               </div>
               <div className="relative text-xs leading-[160%] font-medium text-gray-200 inline-block min-w-[39px] z-[1]">

0a6a649e9905f741aa677a992f20d87e56f47fc4
Areesha <126898750+Areesha213@users.noreply.github.com>
2024-07-02 12:09:14 +0500

    Update HomePage.jsx

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -56,67 +56,67 @@ export default function HomePage() {
      </main>
     </div>
 
-    <div>
-    <section
-      className={`self-stretch flex flex-row items-start justify-center py-0 pr-[23px] pl-5 box-border max-w-full shrink-0 text-left text-29xl text-primary-text font-h2 `}
-    >
-      <div className="w-[1283px] flex flex-col items-end justify-start gap-[64px] max-w-full mq450:gap-[16px] mq750:gap-[32px]">
-        <div className="self-stretch flex flex-row items-start justify-start gap-[96px] max-w-full mq450:gap-[24px] mq750:gap-[48px] mq1050:flex-wrap">
-          <div className="flex-1 flex flex-col items-start justify-start gap-[74px] min-w-[460px] max-w-full mq450:gap-[18px] mq750:gap-[37px] mq750:min-w-full">
-            <div className="flex flex-row items-start justify-start pt-0 px-0 pb-[30px] mt-9">
-              <img
-                className="h-[19px] w-[246px] relative object-contain"
-                loading="lazy"
-                alt=""
-                src={img2}
-              />
-            </div>
-            <div className="self-stretch flex flex-row items-start justify-end max-w-full">
-              <div className="w-[608px]  flex flex-col items-start justify-start  max-w-full">
-                <div className="self-stretch flex flex-row items-start justify-start  pr-0 pl-px  pb-24 box-border max-w-full">
-                  <h1 className="m-0 h-[116px] text-5xl md:text-10xl leading-[120%] font-normal font-inherit inline-block max-w-full md:leading-[35px] xl:text-19xl xl:leading-[46px]">
-                    We help underprivileged sections of society to uplift
-                  </h1>
+   <div>
+        <section
+          className={`self-stretch flex flex-row items-start justify-center py-0 pr-[23px] pl-5 box-border max-w-full shrink-0 text-left text-primary-text font-h2 `}
+        >
+          <div className="flex flex-col items-end justify-start  max-w-full ">
+            <div className="self-stretch flex flex-row items-start justify-start gap-[96px] max-w-full  mq1050:flex-wrap">
+              <div className="flex-1 flex flex-col items-start justify-start gap-[20px] max-w-full ">
+                <div className="flex flex-row items-start justify-start pt-0 px-0 pb-[30px] mt-9">
+                  <img
+                    className="h-[19px] w-[246px] relative object-contain"
+                    loading="lazy"
+                    alt=""
+                    src={img2}
+                  />
                 </div>
-                <div className="self-stretch h-[130px]  relative text-base leading-[160%] text-secondary-text inline-block">
-                  <p className="m-0">
-                    We work for the development of the Bastis in the Varanasi
-                    region of UP, helping in education, health and finance.
-                  </p>
-                  <p className="m-0">
-                    We continuously organizes events and awareness sessions in
-                    those areas, making them aware of government schemes,
-                    scholarships and health. We aims to uplift those
-                    underprivileged sections of society.
-                  </p>
+                <div className="self-stretch flex flex-row items-start justify-end max-w-full">
+                  <div className=" flex flex-col items-start justify-start max-w-full">
+                    <div className="self-stretch flex flex-row items-start justify-start pr-0 pl-px pb-4 box-border max-w-full">
+                      <h1 className="m-0 h-[116px] text-5xl md:text-10xl leading-[120%] font-normal font-inherit inline-block max-w-full  mq450:text-xl mq450:font-bold">
+                        We help underprivileged sections of society to uplift
+                      </h1>
+                    </div>
+                    <div className="self-stretch h-[130px] relative text-base  text-secondary-text inline-block">
+                      <p className="m-0">
+                        We work for the development of the Bastis in the
+                        Varanasi region of UP, helping in education, health and
+                        finance.
+                      </p>
+                      <p className="m-0">
+                        We continuously organizes events and awareness sessions
+                        in those areas, making them aware of government schemes,
+                        scholarships and health. We aims to uplift those
+                        underprivileged sections of society.
+                      </p>
+                    </div>
+                  </div>
+                </div>
+                <div className="w-[346px] flex flex-row items-start justify-start  box-border max-w-full mq450:mt-40">
+                  <button
+                    className="cursor-pointer [border:none] px-8 bg-ghostwhite [backdrop-filter:blur(80px)] rounded flex flex-row items-start justify-start whitespace-nowrap hover:bg-lightgray"
+                    onClick={onSecondaryButtonClick}
+                  >
+                    <a className="[text-decoration:none] text-base font-medium font-h2 text-black1 text-right bg-gray-200 p-4 rounded">
+                      Learn more
+                    </a>
+                  </button>
                 </div>
               </div>
-            </div>
-            <div className="w-[346px] flex flex-row items-start justify-start px-16 box-border max-w-full">
-              <button
-                className="cursor-pointer [border:none]  px-8 bg-ghostwhite [backdrop-filter:blur(80px)] rounded flex flex-row items-start justify-start whitespace-nowrap hover:bg-lightgray"
-                onClick={onSecondaryButtonClick}
-              >
-                <a className="[text-decoration:none]  text-base font-medium font-h2 text-black1 text-right bg-gray-200 p-4 rounded" >
-                  Learn more
-                </a>
-              </button>
+              <div className="h-[588.3px] flex flex-col items-start justify-start mt-32 pt-2.5 px-0 pb-0 box-border min-w-[480px] max-w-full mq750:min-w-full mq1050:flex-1">
+                <img
+                  className="self-stretch flex-1 relative max-w-full overflow-hidden max-h-full mq1050:self-stretch mq1050:w-auto"
+                  loading="lazy"
+                  alt=""
+                  src={img3}
+                />
+              </div>
             </div>
           </div>
-          <div className="h-[588.3px] w-[480px] flex flex-col items-start justify-start mt-32 pt-2.5 px-0 pb-0 box-border min-w-[480px] max-w-full mq750:min-w-full mq1050:flex-1">
-            <img
-              className="self-stretch flex-1 relative max-w-full overflow-hidden max-h-full mq1050:self-stretch mq1050:w-auto"
-              loading="lazy"
-              alt=""
-              src={img3}
-            />
-          </div>
-        </div>
-        <Supporters  />
-        
+        </section>
       </div>
-    </section>
-    </div>
+      <Supporters  />
     <Work className='bg-gray-200 mt-6'/>
     <Projects />
     <Stats className='bg-gray-200 mt-6' />
